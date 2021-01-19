a5e57365bfc674dce538151450364afc90a727a3
erickcartiady <erickcartiady@gmail.com>
2021-01-19 11:57:55 +0700

    :heavy_plus_sign: add bootstrap

---
+++ resources/views/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset("css/app.css")}}">
+    <link rel="stylesheet" href="{{asset("css/style.css")}}">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container-fluid">
+        <nav class="d-flex">
+            <h1>alphabook</h1>
+            <div>
+                <input type="text">
+            </div>
+        </nav>
+    </div>
+</body>
+</html>
